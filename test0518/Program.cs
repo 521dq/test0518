d3701f39bbfb9b5c33b19cf237e3fc54e52bfc19
DuQing <duqing@qinyuantec.com>
2021-05-18 16:00:19 +0800

    0518

---
+++ test0518/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test0518
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
