268ff974b3da50f06ead43e890c49983e09904fc
DuQing <duqing@qinyuantec.com>
2021-05-21 13:57:34 +0800

    0521

---
+++ test0518/Divide.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test0518
+{
+    public class Divide
+    {
+        private double _dataX;
+        private double _dataY;
+        private double _dataZ;
+        public int TestNum { get; set; }
+        public Divide()
+        {
+            _dataX = 1;
+            _dataY = 1;
+            _dataZ = 1;
+        }
+
+        public void setData(double x, double y)
+        {
+            if (y == 0)
+                throw new Exception("y is 0");
+            _dataX = x;
+            _dataY = y;
+        }
+
+        public double getResult()
+        {
+            _dataZ = _dataX / _dataY;
+            return _dataZ;
+        }
+    }
+}
--- test0518/Program.cs
+++ test0518/Program.cs
@@ -6,12 +6,25 @@ namespace test0518
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test 2!");
+            Console.WriteLine("Test!");
             try
             {
-                string testData1 = Console.ReadLine();
-                char[] x = testData1.ToCharArray();
-                Console.WriteLine("input string length: {0}", x.Length);
+                Console.WriteLine("input test num!");
+                string inputData = Console.ReadLine();
+                int testNum = Convert.ToInt32(inputData);
+                Divide testDivide = new Divide();
+                testDivide.TestNum = testNum;
+                for (int i = 1; i <= testDivide.TestNum; i++)
+                {
+                    Console.WriteLine("Test num {0}: input x and y!", i.ToString());
+                    inputData = Console.ReadLine();
+                    double testDataX = Convert.ToDouble(inputData);
+                    inputData = Console.ReadLine();
+                    double testDataY = Convert.ToDouble(inputData);
+                    testDivide.setData(testDataX, testDataY);
+                    double testResult = testDivide.getResult();
+                    Console.WriteLine("output divide result: {0}", testResult.ToString());
+                }
             }
             catch(Exception err)
             {
